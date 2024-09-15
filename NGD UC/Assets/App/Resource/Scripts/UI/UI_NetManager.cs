ced7f38e6da752d073021c105dcf38a298da71bc
Blenisj <148898744+Blenisj@users.noreply.github.com>
2024-09-15 15:51:47 -0400

    Module 3
    added client transform script,
    added player count feature
    added basic ownership p[layer movement,
    added start button for host/server

--- NGD UC/Assets/App/Resource/Scripts/UI/UI_NetManager.cs
+++ NGD UC/Assets/App/Resource/Scripts/UI/UI_NetManager.cs
@@ -6,31 +6,42 @@ using UnityEngine.UI;
 
 public class UI_NetManager : MonoBehaviour
 {
-   [SerializeField] private Button _serverBtn, _clientBtn, _hostBtn;
+   [SerializeField] private Button _serverBtn, _clientBtn, _hostBtn, _startBtn;
+
+   [SerializeField] private GameObject _connectionBttnGroup;
+
+   [SerializeField] private SpawnController _mySpawnController;
 
 void Start()
 {
+    _startBtn.gameObject.SetActive(false);
     _serverBtn.onClick.AddListener(OnServerBtnClicked);
     _clientBtn.onClick.AddListener(OnClientBtnClicked);
     _hostBtn.onClick.AddListener(OnHostBtnClicked);
+    _startBtn.onClick.AddListener(OnStartBtnClicked);
 }
 
 private void OnServerBtnClicked()
 {
     NetworkManager.Singleton.StartServer();
-      this.gameObject.SetActive(false);
+      _connectionBttnGroup.SetActive(false);
+      _startBtn.gameObject.SetActive(true);
 }
 private void OnClientBtnClicked()
 {
     NetworkManager.Singleton.StartClient();
-      this.gameObject.SetActive(false);
+      _connectionBttnGroup.SetActive(false);
 
 }
 private void OnHostBtnClicked()
 {
     NetworkManager.Singleton.StartHost();
-    this.gameObject.SetActive(false);
+    _connectionBttnGroup.SetActive(false);
+    _startBtn.gameObject.SetActive(true);
 }
-
-
+private void OnStartBtnClicked()
+{
+    _mySpawnController.SpawnAllPlayers();
+    _startBtn.gameObject.SetActive(false);
+}
 }
